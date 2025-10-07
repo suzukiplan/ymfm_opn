159968996bdb05c9ebbee09cb1f4536e1f062e59
suzukiplan <suzukiplan.ys@gmail.com>
2025-10-07 11:42:29 +0900

    loading test

--- Makefile
+++ Makefile
@@ -1,4 +1,5 @@
 all: example
+	./example test.vgm
 
 example: example.cpp ymfm_ym2612.hpp
 	g++ -o example example.cpp
--- example.cpp
+++ example.cpp
@@ -1,9 +1,68 @@
+#include <string>
+#include <map>
+#include <vector>
+#include <iostream>
+#include <fstream>
 #include "ymfm_ym2612.hpp"
+
+std::map<int, std::string> chips = {
+    {0x2C, "YM2612"},
+    {0x30, "YM2151"},
+    {0x38, "Sega_PCM"},
+    {0x40, "RF5C68"},
+    {0x44, "YM2203"},
+    {0x48, "YM2608"},
+    {0x4C, "YM2610/B"},
+    {0x50, "YM3812"},
+    {0x54, "YM3526"},
+    {0x58, "Y8950"},
+    {0x5C, "YMF262"},
+    {0x60, "YMF278B"},
+    {0x64, "YMF271"},
+    {0x68, "YMZ280B"},
+    {0x6C, "RF5C164"},
+    {0x70, "PWM"},
+    {0x74, "AY8910"},
+    {0x80, "GB_DMG"},
+    {0x84, "NES_APU"},
+    {0x88, "MultiPCM"},
+    {0x8C, "uPD7759"},
+    {0x90, "OKIM6258"},
+    {0x98, "OKIM6295"},
+    {0x9C, "uPD7759"},
+    {0xA0, "K054539"},
+    {0xA4, "HuC6280"},
+    {0xA8, "C140"},
+    {0xAC, "K053260"},
+    {0xB0, "Pokey"},
+    {0xB4, "QSound"},
+    {0xB8, "SCSP"},
+    {0xC0, "WonderSwan"},
+    {0xC4, "VSU"},
+    {0xC8, "SAA1099"},
+    {0xCC, "ES5503"},
+    {0xD0, "ES5506"},
+    {0xD8, "X1-010"},
+    {0xDC, "C352"},
+    {0xE0, "GA20"},
+    {0xE4, "Mikey"},
+};
+
+std::vector<std::string> chips_supported = {"YM2612"};
 
 class VgmDriver : public ymfm::ymfm_interface
 {
   private:
     ymfm::ym2612 fm;
+
+    struct Context {
+        uint32_t version;
+        const uint8_t* data;
+        size_t size;
+        std::map<std::string, uint32_t> clocks;
+        int32_t cursor;
+        int32_t loopOffset;
+    } vgm;
 
   public:
     VgmDriver() : fm(*this)
@@ -16,6 +75,63 @@ class VgmDriver : public ymfm::ymfm_interface
 
     void reset() { this->fm.reset(); }
 
+    bool load(const uint8_t* data, size_t size)
+    {
+        this->reset();
+        if (size < 0x100) {
+            return false;
+        }
+        if (0 != memcmp("Vgm ", data, 4)) {
+            return false;
+        }
+
+        memcpy(&vgm.version, &data[0x08], 4);
+        if (vgm.version < 0x161) {
+            return false;
+        }
+
+        vgm.data = data;
+        vgm.size = size;
+
+        bool detect_unsupported = false;
+        bool detect_supported = false;
+        for (int i = 0x2C; i < 0xE8; i += 4) {
+            auto it = chips.find(i);
+            if (it != chips.end()) {
+                uint32_t clocks;
+                memcpy(&clocks, &data[it->first], 4);
+                if (clocks) {
+                    printf("Detected %s clocks: %uHz ", it->second.c_str(), clocks);
+                    bool detect = false;
+                    for (auto chip : chips_supported) {
+                        if (chip == it->second) {
+                            detect = true;
+                            break;
+                        }
+                    }
+                    if (detect) {
+                        printf("<supported>\n");
+                        this->vgm.clocks[it->second] = clocks;
+                        detect_supported = true;
+                    } else {
+                        printf("<unsupported!>\n");
+                        detect_unsupported = true;
+                    }
+                }
+            }
+        }
+        if (detect_unsupported || !detect_supported) {
+            return false;
+        }
+
+        memcpy(&vgm.cursor, &data[0x34], 4);
+        vgm.cursor += 0x40 - 0x0C;
+        memcpy(&vgm.loopOffset, &data[0x1C], 4);
+        vgm.loopOffset += vgm.loopOffset ? 0x1C : 0;
+        return true;
+    }
+
+  public:
     //
     // timing and synchronizaton
     //
@@ -53,7 +169,26 @@ class VgmDriver : public ymfm::ymfm_interface
 
 int main(int argc, char* argv[])
 {
+    if (argc < 2) {
+        puts("usage: example /path/to/file.vgm");
+        return 1;
+    }
+    std::ifstream in(argv[1], std::ios::binary | std::ios::ate);
+    if (!in) {
+        puts("File open error!");
+    }
+    std::streamsize size = in.tellg();
+    in.seekg(0, std::ios::beg);
+    std::vector<uint8_t> buf(size);
+    if (!in.read(reinterpret_cast<char*>(buf.data()), size)) {
+        puts("File not found!");
+        return -1;
+    }
+
     VgmDriver vgmdrv;
-    vgmdrv.reset();
+    if (!vgmdrv.load(buf.data(), buf.size())) {
+        puts("Load failed!");
+        return -2;
+    }
     return 0;
 }
